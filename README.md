e559efda47769b781391897ac72110f341f47ba6
Richard Cyrus <richard.cyrus@rcyrus.com>
2024-08-17 00:55:15 -0400

    add mobile styles

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
 # Frontend Mentor - Tip calculator app solution
 
 This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Frontend Mentor - Tip calculator app solution](#frontend-mentor---tip-calculator-app-solution)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [The challenge](#the-challenge)
+  - [My process](#my-process)
+    - [Built with](#built-with)
+    - [Useful resources](#useful-resources)
+  - [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+### Useful resources
+
+- [CSS Button Styling Guide](https://moderncss.dev/css-button-styling-guide/)
+- [Custom CSS Styles for Form Inputs and Textareas](https://moderncss.dev/custom-css-styles-for-form-inputs-and-textareas/)
+- [Inclusively Hidden](https://css-tricks.com/inclusively-hidden/)
+- [Customize Radio Button Appearance with CSS](https://www.markheath.net/post/customize-radio-button-css)
+- [Pure CSS Custom Styled Radio Buttons](https://moderncss.dev/pure-css-custom-styled-radio-buttons/)
+
+## Author
+
+- Website - [Richard Cyrus](https://www.richardcyrus.com)
+- Frontend Mentor - [@richardcyrus](https://www.frontendmentor.io/profile/richardcyrus)
--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,144 @@
 import '~/App.css'
-import splitterLogo from '~/assets/logo.svg'
+import SplitterLogo from '~/assets/logo.svg?react'
 
 function App() {
   return (
     <>
       <header className="logo-container">
-        <img src={splitterLogo} className="logo splitter" alt="Splitter logo" />
+        <SplitterLogo className="logo splitter" />
+        <h1 className="sr-only">Splitter</h1>
       </header>
       <div id="calculator" className="container">
-        <form action="" method="post" id="tip-calculator">
-          <div className="splitter-inputs">
-            <div className="form-control">
-              <label htmlFor="bill-amount">Bill</label>
-              <input
-                type="text"
-                name="bill-amount"
-                id="bill-amount"
-                inputMode="decimal"
-                title="Bill amount"
-                placeholder="0"
-              />
-            </div>
-            <div>
-              <fieldset>
-                <legend>Select Tip %</legend>
-                <label>
-                  <input
-                    type="radio"
-                    name="tip-pct"
-                    id="five-percent"
-                    value="5"
-                  />
+        <form
+          action=""
+          method="post"
+          id="tip-calculator"
+          className="splitter-form"
+        >
+          <div className="form-control">
+            <label htmlFor="bill-amount">Bill</label>
+            <input
+              type="text"
+              name="bill-amount"
+              id="bill-amount"
+              inputMode="decimal"
+              title="Bill amount"
+              placeholder="0"
+              className="input currency"
+            />
+          </div>
+          <div>
+            <h2 className="legend">Select Tip %</h2>
+            <div className="tip-selections">
+              <div>
+                <input
+                  type="radio"
+                  name="tip-percent"
+                  id="five-percent"
+                  value="5"
+                />
+                <label htmlFor="five-percent" className="radio-control">
                   5%
                 </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="tip-percent"
-                    id="ten-percent"
-                    value="10"
-                  />
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="tip-percent"
+                  id="ten-percent"
+                  value="10"
+                />
+                <label htmlFor="ten-percent" className="radio-control">
                   10%
                 </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="tip-percent"
-                    id="fifteen-percent"
-                    value="15"
-                  />
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="tip-percent"
+                  id="fifteen-percent"
+                  value="15"
+                />
+                <label htmlFor="fifteen-percent" className="radio-control">
                   15%
                 </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="tip-percent"
-                    id="twentyfive-percent"
-                    value="25"
-                  />
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="tip-percent"
+                  id="twentyfive-percent"
+                  value="25"
+                />
+                <label htmlFor="twentyfive-percent" className="radio-control">
                   25%
                 </label>
-                <label>
-                  <input
-                    type="radio"
-                    name="tip-percent"
-                    value="50"
-                    id="fifty-percent"
-                  />
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="tip-percent"
+                  value="50"
+                  id="fifty-percent"
+                />
+                <label htmlFor="fifty-percent" className="radio-control">
                   50%
                 </label>
-                <label>
-                  <input
-                    type="text"
-                    name="tip-percent"
-                    id="custom-percent"
-                    placeholder="Custom"
-                  />
-                  <span>Custom</span>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  name="custom-tip-percent"
+                  id="custom-percent"
+                  inputMode="numeric"
+                  placeholder="Custom"
+                  className="input-tip"
+                />
+                <label htmlFor="custom-percent">
+                  <span className="sr-only">Custom</span>
                 </label>
-              </fieldset>
+              </div>
             </div>
-            <div className="form-control">
-              <label htmlFor="num-people">Number of People</label>
-              <input
-                type="text"
-                name="num-people"
-                id="num-people"
-                inputMode="numeric"
-                title="Number of people"
-                placeholder="0"
-              />
-            </div>
+          </div>
+          <div className="form-control">
+            <label htmlFor="num-people">Number of People</label>
+            <input
+              type="text"
+              name="num-people"
+              id="num-people"
+              inputMode="numeric"
+              title="Number of people"
+              placeholder="0"
+              className="input people"
+            />
           </div>
         </form>
         <div className="splitter-results">
-          <div className="result-data">
+          <div className="tip-result-data">
             <div className="result-label">
-              <p>Tip Amount</p>
-              <p>/ person</p>
+              <p className="title">Tip Amount</p>
+              <p className="subtitle">/ person</p>
             </div>
             <div className="result-value">
               <p>$0.00</p>
             </div>
           </div>
-          <div className="result-data">
+          <div className="total-result-data">
             <div className="result-label">
-              <p>Total</p>
-              <p>/ person</p>
+              <p className="title">Total</p>
+              <p className="subtitle">/ person</p>
             </div>
             <div className="result-value">
               <p>$0.00</p>
             </div>
           </div>
           <div className="form-control">
-            <button type="reset" form="tip-calculator">
-              Reset
+            <button
+              type="reset"
+              form="tip-calculator"
+              className="button btn-reset"
+            >
+              RESET
             </button>
           </div>
         </div>
